2dd7532c3b57f37c8e40b94983ff37d48f8c15ab
coolpagani <coolpagani@gmail.com>
2022-09-06 19:28:26 +0300

    work_41

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Сколько чисел вы хотите ввести? ");
+int.TryParse(Console.ReadLine(), out int count);
+int CountingPositiveIntegers(int c)
+{
+    Console.WriteLine("Введите ваши числа: ");
+    int howManyTimes = 0;
+    int i = 0;
+    while (i < c)
+    {
+        int.TryParse(Console.ReadLine(), out var numFromUser);
+        if (numFromUser > 0)
+        {
+            howManyTimes++;
+        }
+        i++;
+    }
+    return howManyTimes;
+}
+Console.WriteLine($"Всего положительных чисел: {CountingPositiveIntegers(count)}");
