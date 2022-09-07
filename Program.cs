c1bf4c673c1cd34907de64b1d9650ff7c8d0cf6c
coolpagani <coolpagani@gmail.com>
2022-09-07 18:51:41 +0300

    work_43

--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
-﻿/* Console.WriteLine("Сколько чисел вы хотите ввести? ");
-int.TryParse(Console.ReadLine(), out int count);
-*/
-int CountingPositiveIntegers(int a)
+﻿string IntersectionPoint(float[] array)
 {
-    int howManyTimes = 0;
-    bool stop = true;
-    while (stop)
-    {
-        Console.WriteLine("Введите ваше число: ");
-        int.TryParse(Console.ReadLine(), out var numFromUser);
-        if (numFromUser != a && numFromUser > 0) howManyTimes++;
-        else if (numFromUser == a) stop = false;
-    }
-    return howManyTimes;
+    float x = 0;
+    float y = 0;
+    string final = "";
+    Console.WriteLine("Сначала введите значения k1, k2, b1, b2: ");
+    for (int i = 0; i < 4; i++) float.TryParse(Console.ReadLine(), out array[i]);
+    x = (array[3] - array[2])/(array[0] - array[1]);
+    y = array[0] * x + array[2];
+    final = $"({x}, {y})";
+    return final;
+
 }
-int stop1 = 999;
-Console.WriteLine("Введите в терминал '" + stop1 + "' для прекращения ввода чисел");
-Console.Write($" Положительных чисел: {CountingPositiveIntegers(stop1)}.");
+
+
+
+float[] Array = new float[4];
+Console.Write(IntersectionPoint(Array));
